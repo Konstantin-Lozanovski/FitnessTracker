8a0541072519e0914540c167943bbd28ffc21e7d
Konstantin Lozanovski <lozanovskikoko@gmail.com>
2026-03-16 18:28:12 +0100

    Removed unnecessary buttons in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,12 +19,7 @@ function Header({ user, setUser }) {
         <Link className="navbar-brand" to="/">
           Fitness Tracker
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
@@ -32,11 +27,6 @@ function Header({ user, setUser }) {
             <li className="nav-item">
               <Link className="nav-link" to="/">
                 Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/workouts">
-                Workouts
               </Link>
             </li>
             <li className="nav-item">
